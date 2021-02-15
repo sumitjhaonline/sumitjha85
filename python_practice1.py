96fb8315aec904f5ba591710f55fd6f2b0487595
sumitjha85 <73512674+sumitjha85@users.noreply.github.com>
2021-02-16 01:09:28 +0545

    Create python_practice1.py

---
+++ python_practice1.py
@@ -0,0 +1,15 @@
+print("hello world")
+a = 4*2**3
+print(a)
+b = 4*(3-3**2)
+print(a, b)
+print(end="")
+c = ["sumit", "amit"]
+print(c)
+Yourname = "sumit jha"
+Yournumber = 9861756205
+print("hello", Yourname, ". your number is", Yournumber, end="")
+print(" hello sumit!")
+D = input("enter a number")
+E = int(D)**6
+print(D, E)
